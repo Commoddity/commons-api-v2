a8c2dc2c75bdc978d63018d39358c7ffb5bd2ce0
pascalvl <pascal@coinmiles.io>
2021-01-09 02:31:19 -0800

    Fixed failing tests

---
+++ src/service/parliaments/__tests__/service.test.ts
@@ -0,0 +1,13 @@
+// import { EventsService } from "..";
+// import { testBills } from "@test";
+
+describe(`ParliamentsService methods`, () => {
+  // beforeEach(async () => {
+  // });
+
+  describe(`Create Parliament`, () => {
+    it(`Creates a new parliament in the DB `, async () => {
+      (() => undefined)();
+    });
+  });
+});
