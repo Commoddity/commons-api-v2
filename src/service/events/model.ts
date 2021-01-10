22cb3e1723036a742016757492626a7e72be0bcb
pascalvl <pascal@coinmiles.io>
2021-01-09 22:18:01 -0800

    Finished writing tests and methods for events service.

--- src/service/events/model.ts
+++ src/service/events/model.ts
@@ -1,26 +1,29 @@
 import { keys } from "ts-transformer-keys";
 
 export interface EventInterface {
-  id: string | undefined;
-  bill_code: string | undefined;
+  id?: string;
+  bill_code: string;
   title: string;
-  publication_date: string | undefined;
+  publication_date: string;
+  created_at?: Date;
 }
 
 export class Event implements EventInterface {
-  id: string | undefined;
-  bill_code: string | undefined;
+  id?: string;
+  bill_code: string;
   title: string;
-  publication_date: string | undefined;
+  publication_date: string;
+  created_at?: Date;
 
   static getColumnNames(): string[] {
     return keys<EventInterface>();
   }
 
-  constructor({ id, bill_code, title, publication_date }) {
+  constructor({ id, bill_code, title, publication_date, created_at }) {
     this.id = id;
     this.bill_code = bill_code;
     this.title = title;
     this.publication_date = publication_date;
+    this.created_at = created_at;
   }
 }
--- src/service/events/service.ts
+++ src/service/events/service.ts
@@ -1,18 +1,51 @@
 import { BaseService } from "../base-service";
+import { BillsService } from "../bills";
 
 import { Event } from "./model";
 
 export class EventsService extends BaseService<Event> {
   private table = "events";
 
-  async createEvent(event: Event): Promise<Event | undefined> {
+  async createEvent(event: Event): Promise<Event> {
     return await super.createOne({ table: this.table, tableValues: event });
   }
 
-  async createManyEvents(events: Event[]): Promise<Event[] | undefined> {
+  async createManyEvents(events: Event[]): Promise<Event[]> {
     return await super.createMany({
       table: this.table,
       tableValuesArray: events,
     });
+  }
+
+  async updateBillsPassedStatus(events: Event[]): Promise<boolean> {
+    let billsUpdated = false;
+
+    for await (const event of events) {
+      const billHasPassed = !!event.title.includes("Royal Assent");
+      const billHasFailed = !!(
+        event.title.includes("Defeated") ||
+        event.title.includes("Not Proceeded With")
+      );
+
+      if (billHasPassed) {
+        await new BillsService().updateBillPassed({
+          code: event.bill_code,
+          passed: true,
+        });
+        billsUpdated = true;
+      } else if (billHasFailed) {
+        await new BillsService().updateBillPassed({
+          code: event.bill_code,
+          passed: false,
+        });
+        billsUpdated = true;
+      }
+    }
+
+    return billsUpdated;
+  }
+
+  async deleteEvent(bill_code: string): Promise<boolean> {
+    return await super.deleteOne({ table: this.table, where: { bill_code } });
   }
 }
