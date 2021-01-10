d3eccd7149fa212e24dcfeefedb483e4e0ac730a
pascalvl <pascal@coinmiles.io>
2021-01-09 22:29:46 -0800

    Updated server config env to dev instead of development.

--- src/config/server.config.ts
+++ src/config/server.config.ts
@@ -2,14 +2,14 @@ const SERVER_ENV: { [key: string]: { SERVER_PORT: number } } = {
   production: {
     SERVER_PORT: Number(process.env.IWIP_SERVER_PORT) || 4800,
   },
-  preproduction: {
+  test: {
     SERVER_PORT: 4800,
   },
-  development: {
+  dev: {
     SERVER_PORT: 4000,
   },
 };
 
-const serverConfig = SERVER_ENV[process.env.NODE_ENV || "development"];
+const serverConfig = SERVER_ENV[process.env.NODE_ENV || "dev"];
 
 export { serverConfig };
